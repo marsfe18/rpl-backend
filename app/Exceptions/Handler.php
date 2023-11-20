cdb2e465b8df01754093ddb507c1673bdaa647b7
Marsay Febrianto <222112177@stis.ac.id>
2023-11-20 07:31:38 +0700

    exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,17 @@
 
 namespace App\Exceptions;
 
+use BadMethodCallException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use PDOException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
+use Illuminate\Support\Str;
 
 class Handler extends ExceptionHandler
 {
@@ -24,7 +33,55 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            Log::info($e->getMessage());
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        DB::rollBack();
+        if ($e instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'message' => 'This Method is not allowed',
+                'data' => null,
+                'error' => $e->getMessage()
+            ], 405);
+        }
+        if ($e instanceof NotFoundHttpException) {
+            return response()->json([
+                'message' => 'This Route is not found',
+                'data' => null,
+                'error' => $e->getMessage(),
+            ], 404);
+        }
+        if ($e instanceof BadMethodCallException) {
+            return response()->json([
+                'message' => 'Bad Method Called',
+                'data' => null,
+                'error' => $e->getMessage(),
+            ], 404);
+        }
+        if ($e instanceof ValidationException) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        }
+
+        if ($e instanceof ModelNotFoundException) {
+            return response()->json([
+                'message' => 'Resource not found',
+                'error' => $e->getModel()
+            ], 404);
+        }
+
+        if ($e instanceof PDOException) {
+            return response()->json([
+                'message' => 'Something Went Wrong',
+                'error' => Str::before($e->getMessage(), '(Connection:'),
+            ], 404);
+        }
+
+        // return parent::render($request, $e);
     }
 }
