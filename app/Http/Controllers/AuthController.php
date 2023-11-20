bc403210568a7d0836b36abaa95ebdd7f1c2a217
Marsay Febrianto <222112177@stis.ac.id>
2023-11-20 15:40:38 +0700

    tambahan dari iyus

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,11 +8,8 @@ use App\Models\User;
 use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
-use Firebase\JWT\JWT;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Facades\JWTFactory;
-use Tymon\JWTAuth\Exceptions\JWTException;
 
 
 class AuthController extends Controller
@@ -37,34 +34,6 @@ class AuthController extends Controller
         ], 201);
     }
 
-    // public function login(Request $request)
-    // {
-    //     $validated = $request->validate([
-    //         'username' => 'required',
-    //         'password' => 'required',
-    //     ]);
-    //     $username = $validated['username'];
-    //     $password = $validated['password'];
-
-    //     if (Auth::attempt(['username' => $username, 'password' => $password])) {
-    //         $user = Auth::user();
-    //         $role = Role::find($user->role_id);
-    //         $roleName = $role ? $role->nama : 'Unknown';
-
-    //         $request->session()->put('username', $user->username);
-    //         $request->session()->put('role', $roleName);
-    //         $apiToken = $user->createToken('api_token')->plainTextToken;
-
-    //         return response()->json([
-    //             'access_token' => $apiToken,
-    //             'token_type' => 'Bearer',
-    //             'role' => $roleName,
-    //         ], 201);
-    //     } else {
-    //         return response()->json(['error' => 'Username atau password salah'], 401);
-    //     }
-    // }
-
     public function login(Request $request)
     {
         $validated = $request->validate([
@@ -85,90 +54,29 @@ class AuthController extends Controller
         $role = Role::find($user->role_id);
         $roleName = $role ? $role->nama : 'Unknown';
 
-        // Buat payload untuk token JWT
-        $payload = [
-            'sub' => $user->id,
-            'role' => $roleName,
-        ];
-
-        // Jika peran adalah PUSKESMAS atau POSYANDU, tambahkan instansi ke payload
         if ($roleName === 'PUSKESMAS' || $roleName === 'POSYANDU') {
             $instansi = ($roleName === 'PUSKESMAS') ? Puskesmas::where('user_id', $user->id)->first() : Posyandu::where('user_id', $user->id)->first();
-            $idInstansi = $instansi ? $instansi->id : null;
-            $payload['instansi'] = $idInstansi;
+            $instansiName = $instansi ? $instansi->nama : null;
+            $payload = JWTFactory::sub($user->id)
+                ->role($roleName)
+                ->user($user)
+                ->instansi($instansiName)
+                ->make();
+        } else {
+            $payload = JWTFactory::sub($user->id)
+                ->role($roleName)
+                ->user($user)
+                ->make();
         }
-
-        $payload = JWTFactory::sub($user->id)
-            ->myCustomString('Foo Bar')
-            ->myCustomArray(['Apples', 'Oranges'])
-            ->myCustomObject($user)
-            ->make();
-
 
         // Generate token JWT dengan payload yang dibuat
         $token = JWTAuth::encode($payload);
-
-        // return response()->json(['token' => $token]);
-        // $token = JWTAuth::fromUser($user, $payload);
 
         return response()->json([
             'access_token' => (string) $token,
             'token_type' => 'Bearer',
         ], 201);
     }
-
-
-    // public function login(Request $request)
-    // {
-    //     $validated = $request->validate([
-    //         'username' => 'required',
-    //         'password' => 'required',
-    //     ]);
-    //     $username = $validated['username'];
-    //     $password = $validated['password'];
-
-    //     if (Auth::attempt(['username' => $username, 'password' => $password])) {
-    //         $user = Auth::user();
-    //         $role = Role::find($user->role_id);
-    //         $roleName = $role ? $role->nama : 'Unknown';
-
-    //         if ($roleName === 'PUSKESMAS') {
-    //             $instansi = Puskesmas::where('user_id', $user->id)->first();
-    //             $idInstansi = $instansi ? $instansi->id : null;
-    //             $payload = [
-    //                 'sub' => $user->id,
-    //                 'role' => $roleName,
-    //                 'instansi' => $idInstansi,
-    //             ];
-    //         } else if ($roleName === 'POSYANDU') {
-    //             $instansi = Posyandu::where('user_id', $user->id)->first();
-    //             $idInstansi = $instansi ? $instansi->id : null;
-    //             $payload = [
-    //                 'sub' => $user->id,
-    //                 'role' => $roleName,
-    //                 'instansi' => $idInstansi,
-    //             ];
-    //         } else {
-    //             $payload = [
-    //                 'sub' => $user->id,
-    //                 'role' => $roleName,
-    //             ];
-    //         }
-
-    //         $secretKey = 'fiNFINrsnlDifnwpnp';
-
-    //         $token = JWT::encode($payload, $secretKey, 'HS256');
-
-
-    //         return response()->json([
-    //             'access_token' => $token,
-    //             'token_type' => 'Bearer',
-    //         ], 201);
-    //     } else {
-    //         return response()->json(['error' => 'Username atau password salah'], 401);
-    //     }
-    // }
-
 
     public function logout(Request $request)
     {
