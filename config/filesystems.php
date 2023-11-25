1592cd5471688da2c94fad7b7d8b42c9c8c6738b
YULIUS RESTU KRISNA ADI <222112427@stis.ac.id>
2023-11-25 18:32:08 +0700

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,12 +36,22 @@ return [
             'throw' => false,
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        // ],
+
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
             'visibility' => 'public',
-            'throw' => false,
         ],
 
         's3' => [
