2b39a7c2ba2cee45481b5efc2aaf815aac8a01ed
Marsay Febrianto <222112177@stis.ac.id>
2023-12-01 14:19:11 +0700

    berita4

--- config/filesystems.php
+++ config/filesystems.php
@@ -28,6 +28,46 @@ return [
     |
     */
 
+    // 'disks' => [
+
+    //     'local' => [
+    //         'driver' => 'local',
+    //         'root' => storage_path('app'),
+    //         'throw' => false,
+    //     ],
+
+    //     // 'public' => [
+    //     //     'driver' => 'local',
+    //     //     'root' => storage_path('app/public'),
+    //     //     'url' => env('APP_URL').'/storage',
+    //     //     'visibility' => 'public',
+    //     //     'throw' => false,
+    //     // ],
+
+    //     'public' => [
+    //         'driver' => 's3',
+    //         'key' => env('AWS_ACCESS_KEY_ID'),
+    //         'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    //         'region' => env('AWS_DEFAULT_REGION'),
+    //         'bucket' => env('AWS_BUCKET'),
+    //         'url' => env('AWS_URL'),
+    //         'visibility' => 'public',
+    //     ],
+
+    //     's3' => [
+    //         'driver' => 's3',
+    //         'key' => env('AWS_ACCESS_KEY_ID'),
+    //         'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    //         'region' => env('AWS_DEFAULT_REGION'),
+    //         'bucket' => env('AWS_BUCKET'),
+    //         'url' => env('AWS_URL'),
+    //         'endpoint' => env('AWS_ENDPOINT'),
+    //         'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+    //         'throw' => false,
+    //     ],
+
+    // ],
+
     'disks' => [
 
         'local' => [
@@ -36,22 +76,12 @@ return [
             'throw' => false,
         ],
 
-        // 'public' => [
-        //     'driver' => 'local',
-        //     'root' => storage_path('app/public'),
-        //     'url' => env('APP_URL').'/storage',
-        //     'visibility' => 'public',
-        //     'throw' => false,
-        // ],
-
         'public' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
+            'throw' => false,
         ],
 
         's3' => [
