156a2f55ef3a565cc541a0edd354a8bb0f7266c0
Marsay Febrianto <222112177@stis.ac.id>
2023-12-02 22:35:03 +0700

    kader edit jabatan

--- app/Http/Controllers/KaderController.php
+++ app/Http/Controllers/KaderController.php
@@ -112,27 +112,39 @@ class KaderController extends Controller
         if (!$kader) {
             return response()->json(['message' => 'Kader not found'], 404);
         }
-        if ($request->has('jabatan') && $request->jabatan !== $kader->jabatan) {
-            $existingKader = Kader::where('posyandu_id', $request->posyandu_id)
-                ->where('jabatan', $request->jabatan)
-                ->first();
+        // if ($request->has('jabatan') && $request->jabatan !== $kader->jabatan) {
+        //     $existingKader = Kader::where('posyandu_id', $request->posyandu_id)
+        //         ->where('jabatan', $request->jabatan)
+        //         ->first();
 
-            if ($request->jabatan !== 'Anggota' && $existingKader) {
-                return response()->json([
-                    'message' => 'The specified role already exists in the Posyandu.',
-                ], 422);
-            }
+        //     if ($request->jabatan !== 'Anggota' && $existingKader) {
+        //         return response()->json([
+        //             'message' => 'The specified role already exists in the Posyandu.',
+        //         ], 422);
+        //     }
 
-            $existingRoles = Kader::where('posyandu_id', $request->posyandu_id)
-                ->whereIn('jabatan', ['Ketua', 'Sekretaris', 'Bendahara'])
-                ->count();
+        //     $existingRoles = Kader::where('posyandu_id', $request->posyandu_id)
+        //         ->whereIn('jabatan', ['Ketua', 'Sekretaris', 'Bendahara'])
+        //         ->count();
 
-            if ($existingRoles > 0 && in_array($request->jabatan, ['Ketua', 'Sekretaris', 'Bendahara'])) {
-                return response()->json([
-                    'message' => 'Posyandu already has Ketua, Sekretaris, or Bendahara. Cannot change to another of these roles.',
-                ], 422);
-            }
+        //     if ($existingRoles > 0 && in_array($request->jabatan, ['Ketua', 'Sekretaris', 'Bendahara'])) {
+        //         return response()->json([
+        //             'message' => 'Posyandu already has Ketua, Sekretaris, or Bendahara. Cannot change to another of these roles.',
+        //         ], 422);
+        //     }
+        // }
+
+        $exist = Kader::where('posyandu_id', $request->posyandu_id)
+            ->where('jabatan', $request->jabatan)
+            ->count();
+        if ($exist > 0) {
+            return response()->json(['message' => 'Posyandu ini telah memiliki seorang ' . $request->jabatan]);
         }
+
+        $kader->nama = $request->nama;
+        $kader->jabatan = $request->jabatan;
+        $kader->posyandu_id = $request->posyandu_id;
+
 
         $kader->update($request->all());
 
